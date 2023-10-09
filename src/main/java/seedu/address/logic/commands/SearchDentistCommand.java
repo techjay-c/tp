f2a4c25b9b56a6a94f3b7d898fea3af39c44db7d
techjay-c <>
2023-10-09 22:13:27 +0800

    Add Search Dentist Command Feature

---
+++ src/main/java/seedu/address/logic/commands/SearchDentistCommand.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+public class SearchDentistCommand extends Command {
+
+    public static final String COMMAND_WORD = "search-dentist";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all dentists whose names contain any of "
+        + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+        + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+        + "Example: " + COMMAND_WORD + " John Tan";
+
+    private final NameContainsKeywordsPredicate predicate;
+
+    public SearchDentistCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+            String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof SearchDentistCommand)) {
+            return false;
+        }
+
+        SearchDentistCommand otherSearchDentistCommand = (SearchDentistCommand) other;
+        return predicate.equals(otherSearchDentistCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .add("predicate", predicate)
+            .toString();
+    }
+}
