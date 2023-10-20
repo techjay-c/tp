3377a6e59fe2bbc3eaef72fdbe977f9b58442f2d
SynapseProgramming <roald.ong@gmail.com>
2023-10-20 11:29:03 +0800

    Add treatment functions into read only AB

--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -5,6 +5,7 @@ import seedu.address.model.appointments.Appointment;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.dentist.Dentist;
 import seedu.address.model.person.patients.Patient;
+import seedu.address.model.treatment.Treatment;
 
 /**
  * Unmodifiable view of an address book
@@ -33,6 +34,12 @@ public interface ReadOnlyAddressBook {
     ObservableList<Appointment> getAppointmentList();
 
     /**
+     * Returns an unmodifiable view of the treatments list.
+     * This list will not contain any duplicate dentists.
+     */
+    ObservableList<Treatment> getTreatmentList();
+
+    /**
      * Returns an integer which represents the next available patient id
      */
     long getPatientId();
@@ -41,4 +48,6 @@ public interface ReadOnlyAddressBook {
      * Returns an integer which represents the next available dentist id
      */
     long getDentistId();
+
+
 }
