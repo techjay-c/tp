5633ea0617f8d8adbf704d172e40ab751c40755a
qyaner <>
2023-10-22 00:46:35 +0800

    Add in check for patientId in AddAppointmentCommand

--- src/main/java/seedu/address/logic/commands/AddAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/AddAppointmentCommand.java
@@ -39,7 +39,11 @@ public class AddAppointmentCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "New Appointment added: %1$s";
 
-    public static final String MESSAGE_CLASHING_APPOINTMENTS = "This Appointment clashes with a current one.";
+    public static final String MESSAGE_CLASHING_DOCTORS = "This doctor already has a " +
+            "current appointment in the same time slot.";
+
+    public static final String MESSAGE_CLASHING_PATIENTS = "This patient already has a " +
+            "current appointment in the same time slot.";
 
     private final Appointment toAdd;
     private final long dentistId;
@@ -95,8 +99,15 @@ public class AddAppointmentCommand extends Command {
             if (!model.getFilteredAppointmentList().isEmpty()) {
                 for (int i = 0; i < model.getFilteredAppointmentList().size(); i++) {
                     if (model.getFilteredAppointmentList().get(i).getDentist() == dentistId) {
-                        throw new CommandException(MESSAGE_CLASHING_APPOINTMENTS);
+                        throw new CommandException(MESSAGE_CLASHING_DOCTORS);
                     }
+                }
+            }
+
+            if (!model.getFilteredAppointmentList().isEmpty()) {
+                for (int i = 0; i < model.getFilteredAppointmentList().size(); i++) {
+                    if (model.getFilteredAppointmentList().get(i).getPatient() == patientId) {
+                        throw new CommandException(MESSAGE_CLASHING_PATIENTS);                    }
                 }
             }
         }
