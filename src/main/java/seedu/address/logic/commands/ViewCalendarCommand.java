e583064141e0c505bbe4990c225e91f64607eaa7
Ruth Lim <>
2023-10-26 03:48:11 +0800

    Add `ViewCalendarCommand`

---
+++ src/main/java/seedu/address/logic/commands/ViewCalendarCommand.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+
+/**
+ * Displays a calendar view window.
+ */
+public class ViewCalendarCommand extends Command {
+
+    public static final String COMMAND_WORD = "view-calendar";
+
+    public static final String SHOWING_CALENDAR_MESSAGE = "Calendar displayed.";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        return new CommandResult(SHOWING_CALENDAR_MESSAGE);
+    }
+}
+
