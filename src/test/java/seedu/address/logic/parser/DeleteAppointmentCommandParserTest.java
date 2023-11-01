30609da529417055eb55cb7f92d479eaaca03741
qyaner <>
2023-11-01 22:46:43 +0800

    Add DeleteAppointmentCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/DeleteAppointmentCommandParserTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DeleteAppointmentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class DeleteAppointmentCommandParserTest {
+
+    private final DeleteAppointmentCommandParser parser = new DeleteAppointmentCommandParser();
+
+    /*@Test
+    public void parse_validAppointmentId_returnsDeleteAppointmentCommand() throws ParseException {
+        long appointmentId = 1;
+        String args = "1";
+        DeleteAppointmentCommand expectedCommand = new DeleteAppointmentCommand(appointmentId);
+
+        assertParseSuccess(parser, args, expectedCommand);
+    }*/
+
+    @Test
+    public void parse_emptyArgs_throwsParseException() {
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+}
