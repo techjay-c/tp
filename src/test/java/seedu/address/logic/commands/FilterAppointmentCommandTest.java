7e8ade1998e6bc5d3c1ea17b95a97250e7d72cc0
qyaner <>
2023-11-09 18:42:39 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/FilterAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterAppointmentCommandTest.java
@@ -1,15 +1,20 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalAppointments.*;
+import static seedu.address.testutil.TypicalAppointments.APPOINTMENT_ONE;
+import static seedu.address.testutil.TypicalAppointments.APPOINTMENT_TWO;
+import static seedu.address.testutil.TypicalAppointments.getTypicalAddressBook;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -74,7 +79,7 @@ public class FilterAppointmentCommandTest {
     }
 
     @Test
-    public void execute_haveDentistId_AppointmentsFound() {
+    public void execute_haveDentistId_appointmentsFound() {
         String expectedMessage = "Appointments with dentist whose dentist ID is 3 listed.";
         long id = 3;
         FilterAppointmentCommand command = new FilterAppointmentCommand("dentist", id);
@@ -84,7 +89,7 @@ public class FilterAppointmentCommandTest {
         assertEquals(Arrays.asList(APPOINTMENT_TWO), model.getFilteredAppointmentList());
     }
     @Test
-    public void execute_havePatientId_AppointmentsFound() {
+    public void execute_havePatientId_appointmentsFound() {
         String expectedMessage = "Appointments with patient whose patient ID is 2 listed.";
         long id = 2;
         FilterAppointmentCommand command = new FilterAppointmentCommand("patient", id);
